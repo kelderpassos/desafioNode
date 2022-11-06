6df72795c517d2d36ebb2ec19099659b291dcff7
Kelder Passos <kelder.passos@gmail.com>
2022-11-06 17:06:20 -0300

    Fix: wrong status code on userControler.delete

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -50,7 +50,7 @@ module.exports = {
     if (!id) throw new Error('Missing params');
 
     await userService.delete(id);
-
-    return res.status(204).json({ message: 'User deleted' });
+    
+    return res.status(200).json({ message: 'User deleted' });
   },
 };
