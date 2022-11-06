f08795e41080bcc78b91eaa030d9532539f5b2f1
Kelder Passos <kelder.passos@gmail.com>
2022-11-06 18:56:26 -0300

    Feat: token generation with JWT completed

--- backend/src/services/loginService.js
+++ backend/src/services/loginService.js
@@ -1,7 +1,13 @@
 const userModel = require('../models/userModel');
+const auth = require('../utils/auth');
 
 module.exports = {
   validateLogin: async ({ email, password }) => {
-    const user = userModel.checkIfUserAlreadyExists()
+    const user = await userModel.findByEmail(email);
+
+    if (!user) throw new Error('User not found');
+    if (user.password !== password) throw new Error('Invalid Password');
+
+    return auth.createToken({ id: user.id, email: user.email });
   }
 }
--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -6,6 +6,6 @@ const SECRET = process.env.JWT_SECRET;
 module.exports = {
   createToken: (payload) =>
     jwt.sign({ payload }, SECRET, { expiresIn: '1d', algorithm: 'HS256' }),
-  
+
   validateToken: (token = null) => jwt.verify(token, SECRET),
 };
