a7c34850382594684a125d7b727dbdc2cf565e47
Kelder Passos <kelder.passos@gmail.com>
2022-11-06 18:26:30 -0300

    Feat: utils folder created to contain support functions. In this commit there are functions to handle token creation and validation

---
+++ backend/src/utils/auth.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const SECRET = process.env.JWT_SECRET;
+
+module.exports = {
+  createToken: (payload) =>
+    jwt.sign({ payload }, SECRET, { expiresIn: '1d', algorithm: 'HS256' }),
+  
+  validateToken: (token = null) => jwt.verify(token, SECRET),
+};
