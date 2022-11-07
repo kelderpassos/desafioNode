522d48c591c3cf68842e68ffae434add1396f7cf
Kelder Passos <kelder.passos@gmail.com>
2022-11-07 08:57:44 -0300

    Feat: validating the incoming password with the one encrypted and storaged in the db with bcrypt

--- backend/src/services/loginService.js
+++ backend/src/services/loginService.js
@@ -1,12 +1,13 @@
 const userModel = require('../models/userModel');
 const auth = require('../utils/auth');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
   validateLogin: async ({ email, password }) => {
     const user = await userModel.findByEmail(email);
 
     if (!user) throw new Error('User not found');
-    if (user.password !== password) throw new Error('Invalid Password');
+    if (!bcrypt.compareSync(password, user.password)) throw new Error('Invalid Password');
 
     return auth.createToken({ id: user.id, email: user.email });
   }
