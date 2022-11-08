a7f56ac5fb85d742c05d16e44513175e6581d396
Kelder Passos <kelder.passos@gmail.com>
2022-11-08 19:44:00 -0300

    Feat: first template created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,27 @@
 <template>
-  
+
+    <SignupForm />
+
 </template>
+
+<script>
+  import LoginForm from './components/LoginForm.vue';
+  import SignupForm from './components/SignupForm.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      SignupForm
+    }
+  }
+</script>
+
+<style>
+  body {
+    margin: 0;
+    display: flex;
+    place-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+  }
+</style>
