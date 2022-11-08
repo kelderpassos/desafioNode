0e0e07fb72740852aed7092086793e2c616b1370
Kelder Passos <kelder.passos@gmail.com>
2022-11-08 20:42:03 -0300

    Feat: Login form component under development

---
+++ frontend/src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>Acesse a plataforma</h3>
+    <form>
+      <label for="email">E-mail
+        <input type="email" name="email" required v-model="email">
+      </label>
+      <label for="password">Password
+        <input type="password" name="password" required v-model="password">
+      </label>
+    </form>
+    <button type="button" @click="submitForm">Acessar</button>
+    <button type="button">Registrar</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    submitForm() {
+
+    }
+  }
+}
+</script>
