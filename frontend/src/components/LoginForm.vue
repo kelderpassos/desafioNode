1e18910886258d49c87e1bfa6130bbc74e45b71c
Kelder Passos <kelder.passos@gmail.com>
2022-11-09 16:04:04 -0300

    Feat: LoginForm is now able to enter the application and save the token

--- frontend/src/components/LoginForm.vue
+++ frontend/src/components/LoginForm.vue
@@ -2,29 +2,46 @@
   <div>
     <h3>Acesse a plataforma</h3>
     <form>
-      <label for="email">E-mail
-        <input type="email" name="email" required v-model="email">
-      </label>
-      <label for="password">Password
-        <input type="password" name="password" required v-model="password">
-      </label>
+      <div>
+        <label for="email">E-mail</label>
+          <input type="email" name="email" required v-model="email">
+        
+      </div>
+      <div>
+        <label for="password">Password</label>
+          <input type="password" name="password" required v-model="password">        
+      </div>
     </form>
-    <button type="button" @click="submitForm">Acessar</button>
+    <button @click.prevent="submitForm">Acessar</button>
     <button type="button">Registrar</button>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'LoginForm',
   data() {
     return {
-      
+      email:'',
+      password:'',
+      baseUrl: 'http://localhost:3001/',
+      token: ''
     }
   },
   methods: {
     submitForm() {
+      console.log(`${this.baseUrl}/login`);
+      axios.post(`http://localhost:3001/login`, { email: this.email, password: this.password })
+        .then((res) => {
+          const token = JSON.stringify(res.data);
+          localStorage.setItem('token', token);
 
+          this.$router.push('/users');          
+        });
+
+      // fetch('http://localhost:3001/users')
     }
   }
 }
