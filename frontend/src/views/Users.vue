1086cd4e85755ba1d9e7544a3f198dd11835e7e2
Kelder Passos <kelder.passos@gmail.com>
2022-11-09 16:36:55 -0300

    Feat: route /users fetchs all users in the database

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,22 +1,37 @@
 <template>
   <main>
     <h2>Todos os usuários</h2>
-    <div v-for="user in users" :key="user.id"> </div>
+    <div v-for="user in users" :key="user.id">
+      <p>{{ user.name }}</p>
+      <p>{{ user.email }}</p>
+    </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  name: 'Users',
   data() {
     return {
       users: []
     }
   },
   mounted() {
-    fetch('http://localhost:3001/users')
-      .then((res) => res.json())
-      .then((data) => this.users = data)
+    const token = JSON.parse(localStorage.getItem('token'));
+
+    const config = {
+      headers: {
+        authorization: token
+      },
+    }
+    axios('http://localhost:3001/users', config)
+      .then((res) => this.users = res.data)
       .catch(error => console.error(error.message));
+  },
+  methods: {
+
   }
 }
 </script>
