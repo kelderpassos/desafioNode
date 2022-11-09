6adbab9d098b6390bd70b88e6233c21e267cd64b
Kelder Passos <kelder.passos@gmail.com>
2022-11-09 07:27:15 -0300

    Feat: page /users under development

---
+++ frontend/src/views/Users.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <h2>Todos os usuários</h2>
+    <div v-for="user in users" :key="user.id"> </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3001/users')
+      .then((res) => res.json())
+      .then((data) => this.users = data)
+      .catch(error => console.error(error.message));
+  }
+}
+</script>
+
+<style>
+
+</style>
