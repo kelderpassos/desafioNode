8d6ae52fde9033787225b731ca06234cbff5b5ed
Kelder Passos <kelder.passos@gmail.com>
2022-11-09 16:36:12 -0300

    Fix: name of the component put correctly

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '../components/LoginForm.vue';
 import SignupForm from '../components/SignupForm.vue';
+import Users from '../views/Users.vue'
 
 const routes = [
   { path: '/', name: 'LoginForm', component: LoginForm },
   { path: '/signup', name: 'SignupForm', component: SignupForm },
-  { path: '/users', name: 'Teste', component: LoginForm }
+  { path: '/users', name: 'Users', component: Users }
 ];
 
 const router = createRouter({
