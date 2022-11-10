6651547cedf0b41c574fe4e7bdda59792ead33c9
Kelder Passos <kelder.passos@gmail.com>
2022-11-09 22:09:13 -0300

    Feat: more routes added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginForm from '../components/LoginForm.vue';
 import SignupForm from '../components/SignupForm.vue';
-import Users from '../views/Users.vue'
+import Users from '../views/Users.vue';
+import UserDetails from '../views/UserDetails.vue';
 
 const routes = [
   { path: '/', name: 'LoginForm', component: LoginForm },
   { path: '/signup', name: 'SignupForm', component: SignupForm },
-  { path: '/users', name: 'Users', component: Users }
+  { path: '/users', name: 'Users', component: Users },
+  { path: '/users/:id', name: 'UserDetails', component: UserDetails }
 ];
 
 const router = createRouter({
